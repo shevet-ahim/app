8877ff039a9bfdc454d81c4727e99f5825334e3f
mbassan <moises@organic.com.pa>
2017-01-27 20:17:21 +0000

    Still trying.

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -11,13 +11,14 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+    	alert('habob');
     	if (!window.sa)
     		window.sa = new sa();
     }
