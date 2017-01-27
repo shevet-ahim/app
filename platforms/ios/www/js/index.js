11c46ec5c77978786e70ec9ab3114cdadd1c7e5a
mbassan <moises@organic.com.pa>
2017-01-27 20:12:14 +0000

    Location.

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -18,7 +18,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-    	window.sa = new sa();
+    	if (!window.sa)
+    		window.sa = new sa();
     }
 };
 
