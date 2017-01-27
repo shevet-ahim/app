5c0358fe4bd38bfe644f7b556dfda44a59fe8374
mbassan <moises@organic.com.pa>
2017-01-27 20:21:03 +0000

    Wrong branch.

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -18,7 +18,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-    	alert('habob');
     	if (!window.sa)
     		window.sa = new sa();
     }
