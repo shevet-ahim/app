7e38bbe0d3a24cf4f22c6d507370ac58dfaa1e5a
mbassan <moises@organic.com.pa>
2017-02-22 23:23:04 +0000

    Build.

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -265,7 +265,7 @@ sa.prototype.init = function(){
 				self.resizePanels();
 				self.externalLinks();
 
-				$('#sa-menu').height($('#sa-menu').find('.ui-panel-inner').height());
+				//$('#sa-menu').height($('#sa-menu').find('.ui-panel-inner').height());
 			});
 			
 			// lightbox
@@ -660,7 +660,7 @@ sa.prototype.loadTefilot = function(return_data){
 
 sa.prototype.loadFeed = function(more,preload){
 	$.mobile.loading('show');
-	console.log('asd')
+	
 	var reload = (!this.session.inside && !preload);
 	var start = null;
 	var end = null;
@@ -685,6 +685,9 @@ sa.prototype.loadFeed = function(more,preload){
 	var popups = [];
 	var delay = 0;
 	popups_shown = (!popups_shown) ? [] : popups_shown;
+	
+	if (!old_feed)
+		old_feed = [];
 	
 	if (!more && feed) {
 		delay = 3000;
